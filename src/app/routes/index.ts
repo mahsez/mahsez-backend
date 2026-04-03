577890b148ff14a2835b026d2a326f7ddb90ce00
Abdullah Al Masud <abdullahalmasud772@gmail.com>
2026-04-03 18:05:41 +0600

    another update

---
+++ src/app/routes/index.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import { UserRoute } from '../module/user/user.route.js';
+
+const router: Router = Router();
+
+const moduleRoute = [
+  {
+    path: '/user',
+    route: UserRoute,
+  },
+];
+
+moduleRoute.forEach((route) => router.use(route.path, route.route));
+
+export default router;
